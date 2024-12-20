741262ea989f79c02655aac901d9da42febbec3e
Jorgen Lundman <lundman@lundman.net>
2024-12-20 10:15:44 +0900

    no double messag3
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>

--- .github/workflows/notify-irc.sh
+++ .github/workflows/notify-irc.sh
@@ -9,9 +9,9 @@ commit_count=$(echo "$COMMITS_JSON" | jq 'length')
 commit_count=${commit_count:-0}
 
 if [ "$commit_count" -eq 0 ]; then
-    echo "message=[openzfs] $ACTOR pushed no commits to $BRANCH at $COMPARE_URL"
+    echo "[openzfs] $ACTOR pushed no commits to $BRANCH at $COMPARE_URL"
 else
-    echo "message=[openzfs] $ACTOR pushed $commit_count commits to $BRANCH at $COMPARE_URL"
+    echo "[openzfs] $ACTOR pushed $commit_count commits to $BRANCH at $COMPARE_URL"
 fi
 
 
