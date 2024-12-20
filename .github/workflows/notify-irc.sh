ad5bc25142d09bd09c88d94a5cce5deecafeab90
Jorgen Lundman <lundman@lundman.net>
2024-12-20 11:06:42 +0900

    multiline test
    Signed-off-by: Jorgen Lundman <lundman@lundman.net>

--- .github/workflows/notify-irc.sh
+++ .github/workflows/notify-irc.sh
@@ -11,6 +11,8 @@ COMPARE_URL="${GITHUB_COMPARE_URL:-unknown}"
 commit_count=$(echo "$COMMITS_JSON" | jq 'length')
 commit_count=${commit_count:-0}
 
+echo 'message=Testing line one\\nTest line two' >> $GITHUB_OUTPUT
+exit 0
 
 
 # Create an initial message
